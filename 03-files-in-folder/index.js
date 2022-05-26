286b4e07f361483c1291aca3eecb4519c06fa887
Igor Badyukov <badyukov1806302003@gmail.com>
2022-05-26 11:10:38 +0300

    fix: change output name file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,9 +6,11 @@ const getInfo = async () => {
     try {
         const allFiles = await fs.promises.readdir(fileWay, {withFileTypes: true});
         for (let file of allFiles) {
+            const extensionName = path.extname(path.join(__dirname, `secret-folder/${file.name}`));
+            const fileName = path.basename(path.join(__dirname, `secret-folder/${file.name}`), extensionName);
             if (file.isFile()) {
                 fs.stat(path.join(__dirname, `secret-folder/${file.name}`), (err, stats) => {
-                    console.log(`${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${stats.size}byte`);
+                    console.log(`${fileName} - ${extensionName.slice(1)} - ${stats.size}byte`);
                 })
             }
         }
